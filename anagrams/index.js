6632cf7a7fe51fa021fd4656baa732cee6cb8b8e
Abinash Gupta <P10433833@capita.co.in>
2018-05-08 16:37:06 +0530

    completed anagrams

--- anagrams/index.js
+++ anagrams/index.js
@@ -8,6 +8,46 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+// My solution
+// function anagrams(stringA, stringB) {
+
+//     const strA = stringA.replace(/[^\w]/g, "").toLowerCase();
+//     const strB = stringB.replace(/[^\w]/g, "").toLowerCase();
+
+//     const sortedA = strA.split('').sort().join('');
+//     const sortedB = strB.split('').sort().join('');
+
+//     return sortedA === sortedB
+
+// }
+
+function anagrams(stringA, stringB) {
+    
+        const strA = stringA.replace(/[^\w]/g, "").toLowerCase();
+        const strB = stringB.replace(/[^\w]/g, "").toLowerCase();
+    
+        const mapA = strA.split('').reduce((acc, curr) => {
+            acc.hasOwnProperty(curr) ? acc[curr]++ : acc[curr] = 1
+            return acc
+        }, {})
+    
+        const mapB = strB.split('').reduce((acc, curr) => {
+            acc.hasOwnProperty(curr) ? acc[curr]++ : acc[curr] = 1
+            return acc
+        }, {})
+
+        if (Object.keys(mapA).length !== Object.keys(mapB).length) {
+            return false
+        }
+
+        for (let key in mapA) {
+            if (mapA[key] !== mapB[key]) {
+                return false
+            }
+        }
+
+        return true
+    
+    }
 
 module.exports = anagrams;
