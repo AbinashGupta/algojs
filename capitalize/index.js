0724d5c7368d3bc7c06d3358e23a6afe8cd4204a
Abinash Gupta <P10433833@capita.co.in>
2018-05-09 12:06:48 +0530

    capitalize completed

--- capitalize/index.js
+++ capitalize/index.js
@@ -7,6 +7,33 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+//my solution #1 same as solution #2
+// function capitalize(str) {
+//     const strArray = str.split('');
+//     for (let i=0; i<str.length; i++) {
+//         if (i === 0) {
+//             strArray[i] = strArray[i].toUpperCase();
+//         }
+//         else if(strArray[i] === ' ') {
+//             strArray[i + 1] = strArray[i + 1].toUpperCase();
+//         }
+//     }
 
+//     return strArray.join('');
+// }
+
+//my solution #2 same as solution #1
+function capitalize(str) {
+    const strArray = str.split(' ');
+
+    return strArray.map((word) => {
+        const wordArr = word.split('')
+        wordArr[0] = wordArr[0].toUpperCase();
+        return wordArr.join('')
+    }).join(' ');
+}
+
+function capitalize(str) {
+
+}
 module.exports = capitalize;
