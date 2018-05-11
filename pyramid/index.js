0273937b85cb1eeeb77fef69d0d9568963253c58
Abinash Gupta <P10433833@capita.co.in>
2018-05-11 14:18:11 +0530

    pyramid completed

--- pyramid/index.js
+++ pyramid/index.js
@@ -14,6 +14,39 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+//my solution
+// function pyramid(n) {
+//     const col = 2*n - 1
+//     const mid = n - 1;
+//     const arr = Array(col).fill(' ');
+//     for ( i=0; i < n; i++) {
+//         arr.fill('#', (mid - i), (mid + i) + 1) 
+//         console.log(arr.join(''));
+//     }
+// }
+
+//my solution with hint --> same as solution
+function pyramid(n, row = 0, level = '') {
+
+    const mid = n - 1;
+    if ( row === n ) {
+        return
+    }
+
+    if ( level.length === (2*n - 1)) {
+        console.log(level)
+        return pyramid(n, row + 1)
+    }
+    else if (level.length >= (mid - row) && level.length <= (mid + row)) {
+        level += '#'
+    }
+    else {
+        level += ' '
+    }
+
+    return pyramid(n, row, level)
+
+}
+
 
 module.exports = pyramid;
